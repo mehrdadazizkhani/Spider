3c4d3130da3222751a32ab31393e5d27aa121746
Mehrdad Azizkhani <mehrdad.azizkhani@gmail,com>
2022-08-10 06:13:58 +0430

    Note component fixed and improved

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -4,9 +4,8 @@ import styles from './Loading.module.css'
 const Loading = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.circle}></div>
-            <div className={styles.circle}></div>
-            <div className={styles.circle}></div>
+            <div className={styles.line}></div>
+            <i className="fa-solid fa-spider" id={styles['logo']}></i>
         </div>
     );
 };
