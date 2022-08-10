a2bb89465abf7861d7b71f515f736c37158fde4a
Mehrdad Azizkhani <mehrdad.azizkhani@gmail,com>
2022-08-10 06:09:18 +0430

    Note component fixed and improved

--- src/components/Note.js
+++ src/components/Note.js
@@ -5,19 +5,27 @@ const Note = () => {
 
     const [content, setContent] = useState('')
 
-    const contenHandler = (event) => {
+    const contentHandler = (event) => {
         setContent(prevContent => prevContent = event.target.value)
         wordCounter()
     }
 
     const wordCounter = () => {
-        'salam'
+        let words = content.split(/\s+/g)
+        if (words[words.length - 1] == '') {
+            words.pop()
+        }
+        if (words[0] == '') {
+            words.shift()
+        }
+        return words.length
     }
+
 
     return (
         <div className={styles.note}>
-            <textarea type={'text'} onChange={contenHandler} value={content} placeholder='You can write something here'></textarea>
-            <p className={`${styles.info} ${content ? '' : styles.deactiveContent}`}>words: <span>{wordCounter}</span></p>
+            <textarea type={'text'} onChange={contentHandler} value={content} placeholder='You can write something here'></textarea>
+            <p className={`${styles.info} ${content ? '' : styles.deactiveContent}`}>Number of words: <span>{wordCounter()}</span></p>
         </div>
     );
 };
