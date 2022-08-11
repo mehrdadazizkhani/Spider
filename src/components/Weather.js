ef2f4cd6fb014441d138fe902083a0dc321d2bdd
Mehrdad Azizkhani <mehrdad.azizkhani@gmail,com>
2022-08-11 08:49:32 +0430

    weather module improved

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -6,25 +6,41 @@ import axios from 'axios';
 
 
 const Weather = () => {
-
+    const [data, setData] = useState([])
     const [content, setContent] = useState('')
     const [city, setCity] = useState('tehran')
-    const [temp, setTemp] = useState(0)
-    const [icon, setIcon] = useState()
     const [cityName, setCityName] = useState()
+    const [country, setCountry] = useState()
+    const [flag, setFlag] = useState()
+    const [temp, setTemp] = useState()
+    const [icon, setIcon] = useState()
+    const [descirption, setDescription] = useState()
 
     const apiKey = "ab939675fd2424a38982c7662d33d8af"
    
-    // `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]}.svg`
-
+    useEffect(()=>{
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+        .then(response => {
+            setCityName(prevCityName => prevCityName = response.data.name)
+            setCountry(prevCountry => prevCountry = response.data.sys.country)
+            setFlag(prevFlag => prevFlag = `https://openweathermap.org/images/flags/${response.data.sys.country.toLowerCase()}.png`)
+            setTemp(prevTemp => prevTemp = response.data.main.temp)
+            setIcon(preIcon => preIcon = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
+            setDescription(prevDescription => prevDescription = response.data.weather[0].description)
+            setData(response.data)
+        })
+    }, [])
     
     const submitHandler = () => {
-        let finalCity = content ? content : city 
-        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${finalCity}&appid=${apiKey}&units=metric`)
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${content}&appid=${apiKey}&units=metric`)
         .then(response => {
-            setTemp(prevTemp => prevTemp = response.data.main.temp)
-            setIcon(preIcon => preIcon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${response.data.weather[0]["icon"]}.svg`)
             setCityName(prevCityName => prevCityName = response.data.name)
+            setCountry(prevCountry => prevCountry = response.data.sys.country)
+            setFlag(prevFlag => prevFlag = `https://openweathermap.org/images/flags/${response.data.sys.country.toLowerCase()}.png`)
+            setTemp(prevTemp => prevTemp = response.data.main.temp)
+            setIcon(preIcon => preIcon = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
+            setDescription(prevDescription => prevDescription = response.data.weather[0].description)
+            setData(response.data)
         })
         setContent('')
     }
@@ -33,16 +49,17 @@ const Weather = () => {
 
     return (
         <div className={styles.container}>
-            {/* <Loading/> */}
             <div className={styles.inputGroup}>
-                <input className={styles.input} onChange={event => setContent(event.target.value)} type='text' value={content} placeholder='Search for a city'></input>
+                <input className={styles.input} onChange={event => setContent(event.target.value)} type='text' value={content} placeholder='City ...'></input>
                 <button className={styles.key} onClick={submitHandler}>Submit</button>
             </div>
             <div className={styles.dataContainer}>
-                <h2>{cityName}</h2>
-                <h3>{`${temp} °c`}</h3>
-                <img src={icon}></img>
+            <h2>{cityName} <span>{country}</span> <img src={flag}></img></h2>
+            <h3>{`${temp} °c`}</h3>
+            <img src={icon}></img>
+            <p>{descirption}</p>
             </div>
+
         </div>
     );
     
